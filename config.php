6f609d1f716e4abbbed20971a489b4816d5baf83
ashaban <allyshaban5@gmail.com>
2017-07-24 00:29:55 +0300

    Changes

--- config.php
+++ config.php
@@ -5,9 +5,9 @@ $rapidpro_url = "https://app.rapidpro.io/";
 $csd_host = "http://localhost:8984/CSD/";
 $csd_user = "csd";
 $csd_passwd = "csd";
-$csd_doc = "mhero_blank_ihris";
-$rp_csd_doc = "mhero_blank_rapidpro";
-$eidsr_host = "https://lib-eidsr-dev.ehealthafrica.org/casealert";
+$csd_doc = "mhero_liberia_ihris";
+$rp_csd_doc = "mhero_liberia_rapidpro";
+$eidsr_host = "https://lib-eidsr-stage.ehealthafrica.org/casealert";
 $eidsr_user = "";
 $eidsr_passwd = "";
 ?>
