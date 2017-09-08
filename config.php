a3a3712458dbe94a7b90adb3290701829906807e
ashaban <allyshaban5@gmail.com>
2017-09-08 21:12:23 +0300

    Adding config file

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+$rapidpro_token = "";
+$case_alert_flow_uuid = "";
+$request_pickup_flow_uuid = "";
+$riders_picked_flow_uuid = "";
+$riders_delivered_flow_uuid = "";
+$rapidpro_url = "";
+$csd_host = "http://localhost:8984/CSD/";
+$csd_user = "csd";
+$csd_passwd = "csd";
+$csd_doc = "";
+$rp_csd_doc = "";
+//$eidsr_host = "https://lib-eidsr-stage.ehealthafrica.org/casealert";
+$eidsr_host = "";
+$eidsr_user = "";
+$eidsr_passwd = "";
+$notify_group = array();
+$riders_group = "";
+?>
