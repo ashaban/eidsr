1ef725f2c8f8a0930432468a0ed96d4b817caad8
ashaban <allyshaban5@gmail.com>
2017-09-08 21:17:18 +0300

    Removing unwanted file

--- config.php
+++ config.php
@@ -1,19 +1,19 @@
 <?php
-$rapidpro_token = "";
-$case_alert_flow_uuid = "";
-$request_pickup_flow_uuid = "";
-$riders_picked_flow_uuid = "";
-$riders_delivered_flow_uuid = "";
-$rapidpro_url = "";
+$rapidpro_token = "f69ee75ccad4c86f5470eddc048832ed48d20c7d";
+$case_alert_flow_uuid = "4bc7f6fd-1dc6-42bd-8953-61a37d71bda3";
+$request_pickup_flow_uuid = "495c6def-23d2-4883-a46d-41e99148e87e";
+$riders_picked_flow_uuid = "41e0a46c-61cd-4ae9-a4bc-faf5f584cc1c";
+$riders_delivered_flow_uuid = "f7a9e80f-a3e5-4094-bf75-2484052eb504";
+$rapidpro_url = "http://192.168.43.241:8000/";
 $csd_host = "http://localhost:8984/CSD/";
 $csd_user = "csd";
 $csd_passwd = "csd";
-$csd_doc = "";
-$rp_csd_doc = "";
+$csd_doc = "mhero_liberia_test";
+$rp_csd_doc = "mhero_liberia_rapidpro";
 //$eidsr_host = "https://lib-eidsr-stage.ehealthafrica.org/casealert";
-$eidsr_host = "";
-$eidsr_user = "";
-$eidsr_passwd = "";
-$notify_group = array();
-$riders_group = "";
+$eidsr_host = "https://lib-eidsr-dev.ehealthafrica.org/casealert";
+$eidsr_user = "rapidpro";
+$eidsr_passwd = "l2JxD60ndYcv";
+$notify_group = array("DPC Group","National Reference Lab");
+$riders_group = "Riders Dispatch Testing";
 ?>
